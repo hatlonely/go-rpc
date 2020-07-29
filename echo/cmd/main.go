bb8e69881ecbc4167575074493b99ec71f8b98f7
贺乐 <hatlonely.hl@alibaba-inc.com>
2020-07-30 00:52:14 +0800

    fix idempotent token

---
+++ echo/cmd/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	echo "github.com/hatlonely/go-rpc/echo/api/gen/go/api"
+)
+
+type EchoService struct{}
+
+func (s *EchoService) Echo(ctx context.Context, req *echo.EchoReq) (*echo.EchoRes, error) {
+	return &echo.EchoRes{Value: req.Value}, nil
+}
+
+type CalService struct{}
+
+func (s *CalService) Cal(ctx context.Context, req *echo.CalReq) (*echo.CalRes, error) {
+	var result int64
+	switch req.Info.Op {
+	case "+":
+		result = req.Info.A + req.Info.B
+	case "-":
+		result = req.Info.A - req.Info.B
+	default:
+		return nil, status.Errorf(codes.InvalidArgument, "op should in ['+', '-']")
+	}
+
+	return &echo.CalRes{
+		Result: result,
+		Uid:    req.Uid,
+	}, nil
+}
+
+func main() {
+	mux := runtime.NewServeMux()
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	if err := echo.RegisterEchoServiceHandlerServer(ctx, mux, &EchoService{}); err != nil {
+		panic(err)
+	}
+	if err := echo.RegisterCalServiceHandlerServer(ctx, mux, &CalService{}); err != nil {
+		panic(err)
+	}
+	if err := http.ListenAndServe(":80", mux); err != nil {
+		panic(err)
+	}
+}
