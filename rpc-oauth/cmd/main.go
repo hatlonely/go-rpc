f599d3fb7b80e30e7b6ad2a3d562472a8ba063a3
hatlonely <hatlonely@gmail.com>
2020-11-16 22:12:23 +0800

    update

--- rpc-oauth/cmd/main.go
+++ rpc-oauth/cmd/main.go
@@ -6,18 +6,16 @@ import (
 	"net"
 	"net/http"
 	"os"
-	"time"
 
 	"github.com/gorilla/handlers"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
-	"github.com/hatlonely/go-kit/binding"
+	"github.com/hatlonely/go-kit/bind"
 	"github.com/hatlonely/go-kit/config"
 	"github.com/hatlonely/go-kit/flag"
 	"github.com/hatlonely/go-kit/logger"
 	"github.com/hatlonely/go-kit/refx"
 	"github.com/hatlonely/go-kit/rpcx"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/keepalive"
 
 	"github.com/hatlonely/go-rpc/rpc-oauth/api/gen/go/api"
 	"github.com/hatlonely/go-rpc/rpc-oauth/internal/service"
@@ -34,6 +32,11 @@ type Options struct {
 	Grpc struct {
 		Port int
 	}
+
+	Logger struct {
+		Info logger.Options
+		Grpc logger.Options
+	}
 }
 
 func Must(err error) {
@@ -44,8 +47,8 @@ func Must(err error) {
 
 func main() {
 	var options Options
-	Must(flag.Struct(&options))
-	Must(flag.Parse())
+	Must(flag.Struct(&options, refx.WithCamelName()))
+	Must(flag.Parse(flag.WithJsonVal()))
 	if options.Help {
 		fmt.Println(flag.Usage())
 		return
@@ -58,33 +61,20 @@ func main() {
 	if options.ConfigPath == "" {
 		options.ConfigPath = "config/go-rpc-oauth.json"
 	}
-	cfg, err := config.NewSimpleFileConfig(options.ConfigPath)
+	cfg, err := config.NewConfigWithSimpleFile(options.ConfigPath)
 	if err != nil {
 		panic(err)
 	}
-	Must(binding.Bind(&options, flag.Instance(), binding.NewEnvGetter(binding.WithEnvPrefix("OAUTH")), cfg))
+	Must(bind.Bind(&options, []bind.Getter{flag.Instance(), bind.NewEnvGetter(bind.WithEnvPrefix("OAUTH")), cfg}, refx.WithCamelName()))
 
-	grpcLog, err := logger.NewLoggerWithConfig(cfg.Sub("logger.grpc"), refx.WithCamelName())
+	grpcLog, err := logger.NewLoggerWithOptions(&options.Logger.Grpc)
 	Must(err)
-	infoLog, err := logger.NewLoggerWithConfig(cfg.Sub("logger.info"), refx.WithCamelName())
+	infoLog, err := logger.NewLoggerWithOptions(&options.Logger.Info)
 	Must(err)
 
 	svc, err := service.NewOAuthService()
 	Must(err)
-	rpcServer := grpc.NewServer(
-		rpcx.WithGRPCDecorator(grpcLog),
-		grpc.KeepaliveEnforcementPolicy(keepalive.EnforcementPolicy{
-			MinTime:             5 * time.Second, // If a client pings more than once every 5 seconds, terminate the connection
-			PermitWithoutStream: true,            // Allow pings even when there are no active streams
-		}),
-		grpc.KeepaliveParams(keepalive.ServerParameters{
-			MaxConnectionIdle:     15 * time.Second, // If a client is idle for 15 seconds, send a GOAWAY
-			MaxConnectionAge:      30 * time.Second, // If any connection is alive for more than 30 seconds, send a GOAWAY
-			MaxConnectionAgeGrace: 5 * time.Second,  // Allow 5 seconds for pending RPCs to complete before forcibly closing connections
-			Time:                  5 * time.Second,  // Ping the client if it is idle for 5 seconds to ensure the connection is still active
-			Timeout:               1 * time.Second,  // Wait 1 second for the ping ack before assuming the connection is dead
-		}),
-	)
+	rpcServer := grpc.NewServer(rpcx.WithGRPCDecorator(grpcLog))
 	api.RegisterOAuthServiceServer(rpcServer, svc)
 
 	go func() {
--- rpc-oauth/go.mod
+++ rpc-oauth/go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/golang/protobuf v1.4.3
 	github.com/gorilla/handlers v1.5.1
 	github.com/grpc-ecosystem/grpc-gateway v1.16.0
-	github.com/hatlonely/go-kit v1.0.6
+	github.com/hatlonely/go-kit v1.0.13
 	google.golang.org/genproto v0.0.0-20201030142918-24207fddd1c3
 	google.golang.org/grpc v1.33.1
 )
