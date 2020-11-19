9ef360522400691806c9fb098ecc3695472a0321
hatlonely <hatlonely@gmail.com>
2020-11-19 22:10:19 +0800

    update

--- rpc-cicd/internal/service/service.go
+++ rpc-cicd/internal/service/service.go
@@ -48,7 +48,7 @@ func (s *CICDService) GetTask(ctx context.Context, req *api.GetTaskReq) (*api.Ta
 	defer cancel()
 	var task api.Task
 	if err := collection.FindOne(mongoCtx, bson.M{"_id": objectID}).Decode(&task); err != nil {
-		return nil, errors.Wrap(err, "mongo.Client.FindOne failed")
+		return nil, errors.Wrap(err, "mongo.Collection.FindOne failed")
 	}
 	return &task, nil
 }
@@ -63,7 +63,7 @@ func (s *CICDService) DelTask(ctx context.Context, req *api.DelTaskReq) (*empty.
 	defer cancel()
 	res, err := collection.DeleteOne(mongoCtx, bson.M{"_id": objectID})
 	if err != nil {
-		return nil, errors.Wrap(err, "mongo.Client.DeleteOne failed")
+		return nil, errors.Wrap(err, "mongo.Collection.DeleteOne failed")
 	}
 	rpcx.CtxSet(ctx, "DeleteOneRes", res)
 	return &empty.Empty{}, nil
@@ -75,7 +75,7 @@ func (s *CICDService) PutTask(ctx context.Context, req *api.PutTaskReq) (*empty.
 	defer cancel()
 	res, err := collection.InsertOne(mongoCtx, req.Task)
 	if err != nil {
-		return nil, errors.Wrap(err, "mongo.Client.InsertOne failed")
+		return nil, errors.Wrap(err, "mongo.Collection.InsertOne failed")
 	}
 	rpcx.CtxSet(ctx, "InsertOneRes", res)
 	return &empty.Empty{}, nil
@@ -92,7 +92,7 @@ func (s *CICDService) UpdateTask(ctx context.Context, req *api.UpdateTaskReq) (*
 	req.Task.Id = ""
 	res, err := collection.UpdateOne(mongoCtx, bson.M{"_id": objectID}, bson.M{"$set": req.Task})
 	if err != nil {
-		return nil, errors.Wrap(err, "mongo.Client.UpdateOne failed")
+		return nil, errors.Wrap(err, "mongo.Collection.UpdateOne failed")
 	}
 	rpcx.CtxSet(ctx, "UpdateOneRes", res)
 	return &empty.Empty{}, nil
@@ -104,11 +104,11 @@ func (s *CICDService) ListTask(ctx context.Context, req *api.ListTaskReq) (*api.
 	defer cancel()
 	res, err := collection.Find(mongoCtx, bson.M{}, mopt.Find().SetLimit(req.Limit).SetSkip(req.Offset))
 	if err != nil {
-		return nil, errors.Wrap(err, "mongo.Client.Find failed")
+		return nil, errors.Wrap(err, "mongo.Collection.Find failed")
 	}
 	var tasks []*api.Task
 	if err := res.All(mongoCtx, &tasks); err != nil {
-		return nil, errors.Wrap(err, "mongo.Client.Find.All failed")
+		return nil, errors.Wrap(err, "mongo.Collection.Find.All failed")
 	}
 	return &api.ListTaskRes{Tasks: tasks}, nil
 }
