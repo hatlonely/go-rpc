fd01f61b8ca7e7d9841647e48e39e16fcb630ac4
hatlonely <hatlonely@gmail.com>
2020-12-20 13:50:34 +0800

    update

--- rpc-ancient/deploy/tpl.sh
+++ rpc-ancient/deploy/tpl.sh
@@ -1,8 +1,29 @@
 #!/usr/bin/env bash
 
-function main() {
+function List() {
+    find tpl -maxdepth 1 -name 'environment*' | awk -v FS="." '{if ($1 == "tpl/environment") print $3}'
+}
+
+function Render() {
     mkdir -p tmp
-    gomplate -f "environment.sh.tpl" -c .="$HOME/.gomplate/root.json" > tmp/environment.sh
+    tpl=$1
+    cfg=$2
+    gomplate -f "tpl/environment.sh.${tpl}.tpl" -c .="${cfg}" > "tmp/environment.sh" && echo "render success"
+}
+
+function Help() {
+    echo "sh tpl.sh <action> [env]"
+    echo "example:"
+    echo "  sh tpl.sh ls"
+    echo "  sh tpl.sh render prod ~/.gomplate/prod.json"
+}
+
+function main() {
+    case "$1" in
+        "ls") List;;
+        "render") Render "$2" "$3";;
+        *) Help;;
+    esac
 }
 
 main "$@"
